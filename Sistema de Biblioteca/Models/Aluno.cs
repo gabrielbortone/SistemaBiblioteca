2e89a99fdc405d6597b449b9de255347513919af
Gabriel Bortone <newgabrielbortone@gmail.com>
2020-07-04 17:07:52 -0300

    Adding CPF Validation

--- Sistema de Biblioteca/Models/Aluno.cs
+++ Sistema de Biblioteca/Models/Aluno.cs
@@ -1,4 +1,5 @@
-﻿using Sistema_de_Biblioteca.Models.ValueObjects;
+﻿using Sistema_de_Biblioteca.Models.Validation;
+using Sistema_de_Biblioteca.Models.ValueObjects;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -24,6 +25,7 @@ namespace Sistema_de_Biblioteca.Models
 
         [Display(Name = "Informe o seu número de CPF")]
         [StringLength(11, MinimumLength = 11)]
+        [CustomValidationCPF(ErrorMessage = "CPF inválido")]
         public string CPF { get; set; }
 
 
